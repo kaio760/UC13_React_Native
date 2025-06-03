e1957e0a0515997b3236fcca3516b21df0188a6f
kaio760 <kaiolimacampos517@gmail.com>
2025-06-03 11:56:29 -0300

    Update App.js

--- hoockstate/App.js
+++ hoockstate/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'; 
+import React, { useState, useEffect, useMemo, useRef } from 'react'; 
 import { StyleSheet, Text, View, TextInput, TouchableOpacity, Alert } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
@@ -7,26 +7,62 @@ export default function App() {
   const [nome, setNome] = useState('');
   const [textoDigitado, setTextoDigitado] = useState('');
   const [txtBotao, setTxtBotao] = useState('Entrar');
+  const [userLog, setUserLog] = useState('');
+  const senhaInput = useRef(null);
+
+  useEffect(()=>{
+    async function getStorage() {
+      const nomeStorage = await AsyncStorage.getItem('nomes');
+      if(nomeStorage !== null){
+        userLog(nomeStorage);
+      }
+    }
+    getStorage();
+  },[]);
+
+  useEffect(()=>{
+    async function saveStorage() {
+      await AsyncStorage.setItem('nomes', userLog); 
+    }
+    saveStorage();
+  },[userLog]);
+
+  const letrasNome = useMemo(()=>{
+    return userLog.length;
+    //console.log('Mudou');
+  },[userLog])
 
   function alterarNome() {
     if (textoDigitado === '' && txtBotao === 'Entrar') {
       Alert.alert('Atenção!', 'Nome não digitado!');
       return;
     }
+  
+    if (txtBotao === 'Entrar' && senhaInput.current.value === '') {
+      Alert.alert('Atenção!', 'Senha não digitada!');
+      return;
+    }
 
-    if (txtBotao === 'Sair') {
+    else if (txtBotao === 'Sair') {
       setNome('');
       setTextoDigitado('');
       setTxtBotao('Entrar');
       return;
     }
 
+    
+
+
+
+    setUserLog(textoDigitado);
     setNome(textoDigitado);
+    setTextoDigitado('');
     setTxtBotao('Sair');
+    senhaInput.current.focus();
 
     async function saveStorage() {
-      await AsyncStorage.setItem('nomes', nome);
-      alert('salvo')
+      await AsyncStorage.setItem('nomes', textoDigitado);
+      alert('Nome salvo com sucesso!');
     }
 
     saveStorage();
@@ -34,12 +70,17 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text style={{}}>{nome ? `Seja bem-vindo, ${nome}!` : ''}</Text>
+      <Text style={{marginBottom: 2, color: 'red', fontSize: 13}}>{userLog ? "Usuário Logado: " + userLog : ""}</Text>
+      <Text style={{marginBottom: 8, color: 'red', fontSize: 13}}>{userLog ? "Usuário Logado contém: " + letrasNome + " Letras!" : ""}</Text>
+      <Text style={{marginBottom: 10, fontSize: 18}}>{nome ? "Seja bem-vindo, " + nome + "!" : ''}</Text>
 
-      <TextInput style={{borderWidth:0.8, height:40, margin:10, borderRadius:20}} placeholder="Digite seu nome..." value={textoDigitado} onChangeText={setTextoDigitado}/>
+      <TextInput style={{borderWidth: 0.8, height: 40, margin: 10, borderRadius: 20, paddingHorizontal: 10}} placeholder="Digite seu nome..." value={textoDigitado} onChangeText={setTextoDigitado} />
+      <TextInput style={{borderWidth: 0.8, height: 40, margin: 10, borderRadius: 20, paddingHorizontal: 10}} placeholder="Digite sua senha..." ref={senhaInput} secureTextEntry={true} />
 
-      <TouchableOpacity onPress={alterarNome} style={{backgroundColor: 'darkorange', padding: 10, marginTop:3, borderRadius:10}} >
-        <Text style={{color:'black'}}>{txtBotao}</Text>
+      <TouchableOpacity onPress={alterarNome} 
+        style={{backgroundColor: 'darkorange', padding: 10, marginTop: 10, borderRadius: 10, alignItems: 'center'}} 
+      >
+        <Text style={{color: 'black', fontWeight: 'bold'}}>{txtBotao}</Text>
       </TouchableOpacity>
     </View>
   );
